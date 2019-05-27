94d09ac47efc6f0b37dcdab98e16f70a9b26c304
Allan <allanfodi@hotmail.de>
2019-05-27 15:35:12 +0200

    first commit
    projekt hinzugefügt

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(768, 1024, true);
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GameOVER");
+        gameOverUI.SetActive(true);
+        Invoke("Restart", 3);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //man kann auch verschiedene level laden LoadScene("name")
+    }
+}
---
+++ Assets/Scripts/sidewaysMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sidewaysMovement : Enemy
+{
+    // Start is called before the first frame update
+    private Transform enemyPosition;
+
+
+     
+    void Start()
+    {
+
+        InvokeRepeating("MoveEnemy", 0f, 0.01f);
+        enemyPosition = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void MoveEnemy()
+    {
+        enemyPosition.position += Vector3.right * speed/100;
+        if(enemyPosition.position.x < XminBound || enemyPosition.position.x > XmaxBound)
+        {
+            speed = -speed;
+            enemyPosition.position += Vector3.down * 0.5f;
+            
+        }
+
+
+    }
+}
